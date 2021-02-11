77601ffb8bc9d69b89535a51cdd4d1f6defb0f79
Shay-Lynn Dudhia <55833100+Shay130199@users.noreply.github.com>
2021-02-11 07:59:27 +0000

    Deploy This

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App2.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Tweet() {
+    return (
+        <div className="tweet">
+            <h3> Feel abit Under the weather? </h3>
+            <h3>We are always here to talk....</h3>
+            <ul>Read More</ul>
+        </div>
+
+    );
 }
 
-export default App;
+export default Tweet;
